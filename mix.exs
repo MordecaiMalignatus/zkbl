af6aefe22e696d7a8931df89e5239ae1c15fe931
Az4reus <az4reus@gmail.com>
2016-04-21 11:36:12 +0200

    Adds HTTPoison and Poison dependencies.

--- mix.exs
+++ mix.exs
@@ -14,7 +14,9 @@ defmodule Zkbl.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger,
+                    :httpoison,
+                    :poison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +29,8 @@ defmodule Zkbl.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:dialyze, "~> 0.2.0"}]
+    [{:dialyze, "~> 0.2.0"},
+    {:httpoison, "~> 0.8.0"},
+    {:poison, "~> 2.0"}]
   end
 end
